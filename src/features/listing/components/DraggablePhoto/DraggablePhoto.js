875d5bf043577e9126fc606610d84baf1b7271fe
Alvin Ng <alvinng@Alvin-Ngs-MacBook-Pro.local>
2023-08-10 10:12:59 +0800

    Removed drag indicator

--- src/features/listing/components/DraggablePhoto/DraggablePhoto.js
+++ src/features/listing/components/DraggablePhoto/DraggablePhoto.js
@@ -1,6 +1,5 @@
 import { useFormContext } from "react-hook-form";
 import { FiTrash } from "react-icons/fi";
-import { RiDragMove2Line } from "react-icons/ri";
 import DraggablePhotoCSS from "./DraggablePhoto.module.css";
 
 function DraggablePhoto({ displayValues: { i, url }, inputData: { value } }) {
@@ -20,10 +19,6 @@ function DraggablePhoto({ displayValues: { i, url }, inputData: { value } }) {
         <span className={DraggablePhotoCSS["cover-indicator"]}>COVER</span>
       )}
       <span className={DraggablePhotoCSS["image-number"]}>{i + 1}</span>
-      <RiDragMove2Line
-        size={"30px"}
-        className={DraggablePhotoCSS["drag-indicator"]}
-      />
       <span
         className={DraggablePhotoCSS["delete-btn"]}
         onClick={() => handleDelete()}
