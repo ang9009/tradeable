d19e4fce17c9770fcabf201adf0f15f19508abc3
Alvin Ng <alvinng@Alvin-Ngs-MacBook-Pro.local>
2023-08-11 22:26:15 +0800

    Stable

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,5 @@
+import { FiArrowRight } from "react-icons/fi";
+import Footer from "../../layouts/Footer/Footer";
 import Hero from "../../layouts/Hero/Hero";
 import HomeCSS from "./Home.module.css";
 
@@ -14,7 +16,13 @@ function Home() {
           between members of CIS! Donate, buy, and sell uniforms, textbooks,
           electronics, and more.
         </p>
+        <button className={HomeCSS["arrow-button"]}>
+          Terms & Conditions
+          <FiArrowRight size="17px" className={HomeCSS["arrow"]} />
+        </button>
       </div>
+      <div className={HomeCSS["about-section"]}></div>
+      <Footer />
     </>
   );
 }
