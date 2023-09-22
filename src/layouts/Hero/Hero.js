df9dfe1ce2c544e149a391abcf8946d5c8e7ecba
Alvin Ng <alvinng@pc-193-107.customer.ask4.lan>
2023-09-22 19:09:53 +0100

    Stable

--- src/layouts/Hero/Hero.js
+++ src/layouts/Hero/Hero.js
@@ -1,20 +1,23 @@
+import { useUser } from "../../context/UserContext";
 import HeroCSS from "./Hero.module.css";
 import HeroNavbar from "./components/HeroNavbar/HeroNavbar";
 function Hero() {
+  const { user } = useUser();
+
   return (
     <div className={HeroCSS["hero-container"]}>
       <HeroNavbar />
       <div className={HeroCSS["hero-text"]}>
         <div className={HeroCSS["title-container"]}>
           <span className={HeroCSS["title"]}>
-            Donate, buy, or sell your second-hand items
+            Trade your second-hand items with other students
           </span>
         </div>
         <p className={HeroCSS["desc"]}>
-          Take part in our sustainable initiative! Offer your second-hand items
-          to other members of CIS via tradeable.
+          Take part in our sustainable initiative: sell preloved items to other
+          students in London, or save money by buying them.
         </p>
-        <button className={HeroCSS["fat-btn"]}>Sign up and get started</button>
+        <button className={HeroCSS["fat-btn"]}>Start exploring</button>
       </div>
       <div className={HeroCSS["hero-bg"]}></div>
     </div>
