4c0a5d06f77eb2fd6fc16b072bee6356963ca33c
Alvin Ng <alvinng@Alvin-Ngs-MacBook-Pro.local>
2023-09-28 15:43:57 +0100

    shit

--- src/features/messaging/components/ChatListingInfo/ChatListingInfo.js
+++ src/features/messaging/components/ChatListingInfo/ChatListingInfo.js
@@ -1,18 +1,34 @@
 import { useEffect, useState } from "react";
-import { db, doc, getDoc } from "../../../../lib/firebase";
+import Skeleton from "react-loading-skeleton";
+import {
+  db,
+  doc,
+  getDoc,
+  getDownloadURL,
+  ref,
+  storage,
+} from "../../../../lib/firebase";
 import ChatListingInfoCSS from "./ChatListingInfo.module.css";
 
 function ChatListingInfo({ listingId }) {
   const [listing, setListing] = useState({});
+  const [isFetchingListing, setIsFetchingListing] = useState(true);
 
+  // TODO: dogshit
   useEffect(() => {
     function getListing() {
       const listingRef = doc(db, "listings", listingId);
       getDoc(listingRef).then((res) => {
         if (!res.exists()) {
           setListing(null);
+          setIsFetchingListing(false);
         } else {
           setListing(res.data());
+          const imageRef = ref(storage, `listingImages/${listingId}/1`);
+          getDownloadURL(imageRef).then((res) => {
+            setListing((prev) => ({ ...prev, photoUrl: res }));
+            setIsFetchingListing(false);
+          });
         }
       });
     }
@@ -22,15 +38,32 @@ function ChatListingInfo({ listingId }) {
 
   return (
     <div className={ChatListingInfoCSS["component-container"]}>
-      <img
-        src={require("../../../../assets/authmodal_img.jpg")}
-        alt=""
-        className={ChatListingInfoCSS["listing-img"]}
-      />
-      <div className={ChatListingInfoCSS["listing-details"]}>
-        <p className={ChatListingInfoCSS["listing-name"]}>{listing.name}</p>
-        <p className={ChatListingInfoCSS.price}>£{listing.price}</p>
-      </div>
+      {!isFetchingListing ? (
+        <>
+          <img
+            src={
+              listing
+                ? listing.photoUrl
+                : require("../../../../assets/placeholder_img.jpg")
+            }
+            alt=""
+            className={ChatListingInfoCSS["listing-img"]}
+          />
+          <div className={ChatListingInfoCSS["listing-details"]}>
+            <p
+              className={ChatListingInfoCSS["listing-name"]}
+              style={{ color: listing ? "black" : "red" }}
+            >
+              {listing ? listing.name : "Listing removed"}
+            </p>
+            {listing && (
+              <p className={ChatListingInfoCSS.price}>£{listing.price}</p>
+            )}
+          </div>
+        </>
+      ) : (
+        <Skeleton height={"50px"} />
+      )}
     </div>
   );
 }
