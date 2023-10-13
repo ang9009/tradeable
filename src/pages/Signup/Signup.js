649e05dd3105f2a327a55b62c25bcad106775ac4
Alvin Ng <alvinng@pc-200-255.customer.ask4.lan>
2023-10-13 21:01:03 +0100

    Fixed weird responsiv ebutton issue

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,4 +1,6 @@
+import { useForm } from "react-hook-form";
 import { Navigate } from "react-router-dom";
+import TextInput from "../../components/form/TextInput/TextInput";
 import { useUser } from "../../context/UserContext";
 import { useLogin } from "../../features/auth";
 import SignupCSS from "./Signup.module.css";
@@ -6,13 +8,33 @@ import SignupCSS from "./Signup.module.css";
 function Signup() {
   const { login, error } = useLogin();
   const { user, isFetchingUser } = useUser();
+  const {
+    register,
+    formState: { errors },
+  } = useForm();
 
   return isFetchingUser ? (
     <></>
   ) : !user ? (
     <div className={SignupCSS["sign-up-page-container"]}>
-      <h1 className={`page-title ${SignupCSS["centered-title"]}`}>Sign up</h1>
-      <div className="page-section-container"></div>
+      <h1 className={`${SignupCSS["centered-title"]}`}>Sign up</h1>
+      <TextInput
+        options={{
+          label: "College email",
+          placeholder: "E.g. johndoe@ucl.ac.uk",
+          className: SignupCSS["email-input"],
+        }}
+        formData={{ register, errors }}
+      />
+      <TextInput
+        options={{
+          label: "Password",
+          placeholder: "Enter your password...",
+          className: SignupCSS["password-input"],
+        }}
+        formData={{ register, errors }}
+      />
+      <div className={SignupCSS["divider"]}></div>
     </div>
   ) : (
     <Navigate to="/" />
