f4924191cd4ecec70029f755fc14eeba2950b1d5
Alvin Ng <alvinng@pc-195-159.customer.ask4.lan>
2023-11-01 23:29:33 +0000

    made homepage responsive

--- src/features/listing/components/ListingsCarousel/ListingsCarousel.js
+++ src/features/listing/components/ListingsCarousel/ListingsCarousel.js
@@ -1,14 +1,49 @@
+import { useEffect, useRef } from "react";
+import { register } from "swiper/element";
 import { ListingCard } from "../../../search";
 import ListingsCarouselCSS from "./ListingsCarousel.module.css";
 
 function ListingsCarousel({ listings }) {
+  const swiperRef = useRef(null);
+  useEffect(() => {
+    // Register Swiper web component
+    register();
+
+    // Object with parameters
+    const params = {
+      observer: true,
+      breakpoints: {
+        // when window width is >= 320px
+        // when window width is >= 480px
+        300: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        500: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+      },
+    };
+
+    // Assign it to swiper element
+    Object.assign(swiperRef.current, params);
+
+    // initialize swiper
+    swiperRef.current.initialize();
+  }, []);
+
   return (
     <div className={ListingsCarouselCSS["carousel-container"]}>
       <swiper-container
         class={ListingsCarouselCSS.carousel}
+        ref={swiperRef}
         navigation="true"
-        slides-per-view="4"
-        space-between="15"
       >
         {listings.map((listing) => {
           return (
