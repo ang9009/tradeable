e173a09448c76b737b9f7134663c19cf4aba1e22
Alvin Ng <alvinng@pc-193-131.customer.ask4.lan>
2023-11-11 11:42:46 +0000

    forgot to improt usestate...

--- src/features/messaging/components/ReportModal/ReportModal.js
+++ src/features/messaging/components/ReportModal/ReportModal.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import TextArea from "../../../../components/form/TextArea/TextArea";
