a62b7da792b7f69572d050d1b66800bcafb272a5
Alvin Ng <alvinng@pc-193-131.customer.ask4.lan>
2023-11-11 18:36:43 +0000

    z index issue

--- src/features/search/components/ListingCard/ListingCard.js
+++ src/features/search/components/ListingCard/ListingCard.js
@@ -15,21 +15,6 @@ function ListingCard({ listing }) {
           alt={"Listing image"}
           className={ListingCardCSS["listing-img"]}
         />
-        <div>
-          <div
-            className={ListingCardCSS["status-tag"]}
-            style={{
-              background:
-                listing?.status === "reserved"
-                  ? "var(--reserved-blue)"
-                  : "#fff",
-              color: listing?.status === "reserved" ? "#fff" : "#000",
-            }}
-          >
-            {listing?.status}
-          </div>
-          <div className={ListingCardCSS["overlay"]}></div>
-        </div>
         {listing?.status !== "available" ? (
           <div>
             <div
